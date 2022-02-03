1dad8510fa85bf39afc35d121c09c34e9a11c8db
John Grinwis <john.grinwis@gmail.com>
2022-02-03 13:48:32 +0100

    add new endpoint

--- phased-release/main.tf
+++ phased-release/main.tf
@@ -10,10 +10,10 @@ terraform {
 
 # for cloud usage these vars have been defined in terraform cloud as a set
 # we only need to use this when importing existing resources
-/* provider "akamai" {
+provider "akamai" {
   edgerc         = "~/.edgerc"
   config_section = "betajam"
-} */
+}
 
 # we're storing the state in terraform cloud
 # when using existing resource first import it using 'terraform import akamai_cloudlets_policy.phased_release <cl_policy_name>'
@@ -22,7 +22,7 @@ terraform {
     organization = "grinwis-com"
 
     workspaces {
-      name = "terraform-cloud-akamai-cloudlets"
+      name = "phased-release"
     }
   }
 }
